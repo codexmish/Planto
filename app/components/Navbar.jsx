221159bc5bcf5211f686c72195311b91b55e9946
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-10 09:30:09 +0600

    navber done

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import logo from "../../public/logo-icon.png";
+import { menuitem } from "@/constants";
+import Link from "next/link";
+import { IoSearchOutline } from "react-icons/io5";
+import { PiShoppingCartLight } from "react-icons/pi";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="py-15 absolute top-0 left-0 w-full z-50 bg-red-400 hidden lg:block">
+        <div className="main container flex items-center justify-between">
+          {/* --------logo------- */}
+          <div className="logo flex gap-3 items-center">
+            <Image width={48} height={48} src={logo} />
+            <h1 className=" text-xl text-white font-black font-inter">
+              Planto.
+            </h1>
+          </div>
+
+          {/* -------menu---------- */}
+          <div className="flex items-center gap-15">
+            {menuitem.map((item, id) => (
+              <Link
+                className="text-base text-white font-medium font-inter"
+                key={id}
+                href={item.navLink}
+              >
+                {item.menuName}
+              </Link>
+            ))}
+          </div>
+
+          {/* --------Icons---- */}
+          <div className="text-2xl text-white flex items-center gap-15">
+            <IoSearchOutline className="cursor-pointer" />
+            <PiShoppingCartLight className="cursor-pointer" />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ constants/index.js
@@ -0,0 +1,20 @@
+const menuitem = [
+  {
+    menuName: "Home",
+    navLink: "/",
+  },
+  {
+    menuName: "Plants Type",
+    navLink: "/",
+  },
+  {
+    menuName: "More",
+    navLink: "/",
+  },
+  {
+    menuName: "contact",
+    navLink: "/",
+  },
+];
+
+export { menuitem };
