fb5a8b763caa9623543ff57987158f7feb8eca3a
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-10 10:49:59 +0600

    loading....

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -7,7 +7,13 @@ const Banner = () => {
       <section
         className="main w-full h-[2500px] bg-cover bg-center"
         style={{ backgroundImage: `url(${bgImg.src})` }}
-      ></section>
+      >
+        <div className="container">
+            <div className="left">
+                <h1 className="text-[117px] text-white font-semibold font-inter text-backdrop-blur">Breath Natureal </h1>
+            </div>
+        </div>
+      </section>
     </>
   );
 };
